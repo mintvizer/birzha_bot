4dca4f4be494ecf422b8df6796b6c2cca451e919
mintvizer <mindckol@gmail.com>
2020-12-30 01:45:15 +0700

    Баг фикс второго парсера

--- parser.py
+++ parser.py
@@ -3,6 +3,8 @@ import requests
 from asyncpg import Connection
 from bs4 import BeautifulSoup
 from loader import db
+from datetime import datetime, timedelta
+
 
 
 class DBCommand:
@@ -30,11 +32,18 @@ async def get_html(link):
     return html
 
 async def parse():
-    link = 'https://finviz.com/insidertrading.ashx?tc=7'
+    link = 'https://finviz.com/insidertrading.ashx?tc=1'
     html = await get_html(link)
     soup = BeautifulSoup(html, 'html.parser')
     all_values = await db.get_values_db()
     trs = soup.find('table', class_='body-table').find_all('tr')[1:]
+
+
+    t = datetime.today()
+    today = datetime(t.year, t.month, t.day)
+    # print(today)
+
+
     for i in trs:
         tds = i.find_all('td')
 
@@ -48,13 +57,19 @@ async def parse():
         Value = int(tds[7].text.strip().replace(',', ''))
         shares_total = int(tds[8].text.strip().replace(',', ''))
         sec_form = tds[9].text.strip()
-        if Transaction == "Buy" and Value >= 1000000:
-            print(f'{Ticker} - {Owner} - {Relationship} - {Date} - {Transaction} - {Cost} - {Shares} - {Value} - {shares_total} - {sec_form}')
+
+        # Работа с датой
+        date = datetime.strptime(str(today.year) + Date, '%Y%b %d')
+
+
+
+        if Transaction == "Buy" and Value >= 1000000 and date >= today + timedelta(days=-2):
+            # print(f'{Ticker} - {Owner} - {Relationship} - {Date} - {Transaction} - {Cost} - {Shares} - {Value} - {shares_total} - {sec_form}')
 
             adoption = True
             for item in all_values:
                 if item[1] == Ticker and item[2] == Owner and item[3] == Relationship and item[4] == Date and item[5] == Cost and item[6] == Shares and item[7] == Value and item[8] == shares_total:
-                    print('Точно такая же запись')
+                    # print('Точно такая же запись')
                     adoption = False
                     break
             if adoption:
